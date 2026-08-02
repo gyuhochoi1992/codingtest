3e6f25b07102d6e7b6c2b23309bb9e6f020fe4ce
gyuhochoi1992 <gyuhochoi1992@gmail.com>
2026-08-02 23:29:35 +0900

    [Programmers] 코딩기초트레이닝 Day 10

---
+++ programmers/Lv0/코딩기초트레이닝/Day10/qrcode.py
@@ -0,0 +1,17 @@
+"""
+두 정수 q, r과 문자열 code가 주어질 때, code의 각 인덱스를 q로 나누었을 때 나머지가 r인 위치의 문자를 앞에서부터 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+"""
+
+def solution(q, r, code):
+    return code[r::q]
+    """
+    answer = ''
+    for i in range(len(code)):
+        if i % q == r:
+            answer += code[i]
+    return answer
+    """
+
+if __name__ == '__main__':
+    solution(3, 1, "qjnwezgrpirldywt")
+    solution(1, 0, "programmers")
