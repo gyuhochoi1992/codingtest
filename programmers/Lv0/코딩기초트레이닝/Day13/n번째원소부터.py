ba427cf47ceaa5946abbb76b297981b46a02076d
gyuhochoi1992 <gyuhochoi1992@gmail.com>
2026-08-05 19:39:32 +0900

    [Programmers] 코딩기초트레이닝 Day 13

---
+++ programmers/Lv0/코딩기초트레이닝/Day13/n번째원소부터.py
@@ -0,0 +1,10 @@
+"""
+정수 리스트 num_list와 정수 n이 주어질 때, n 번째 원소부터 마지막 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+"""
+
+def solution(num_list, n):
+    return num_list[n-1:]
+
+if __name__ == '__main__':
+    solution([2, 1, 6], 3)
+    solution([5, 2, 1, 7, 5], 2)
