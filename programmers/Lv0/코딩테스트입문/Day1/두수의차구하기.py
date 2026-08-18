b8ba93aca3ee85a94bb0d5e1acfbfc3e69444e4f
gyuhochoi1992 <gyuhochoi1992@gmail.com>
2026-08-18 20:04:07 +0900

    [Programmers] 코딩테스트 입문 Day 1

---
+++ programmers/Lv0/코딩테스트입문/Day1/두수의차구하기.py
@@ -0,0 +1,13 @@
+"""
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+제한사항
+-50000 ≤ num1 ≤ 50000
+-50000 ≤ num2 ≤ 50000
+"""
+def solution(num1, num2):
+    return num1 - num2
+
+if __name__ == '__main__':
+    solution(2, 3)
+    solution(100, 2)
