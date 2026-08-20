a78f4c2b9bdddb0e9a2ae7f8f94126bd3346ed2b
gyuhochoi1992 <gyuhochoi1992@gmail.com>
2026-08-20 20:32:30 +0900

    [Programmers] 코딩테스트 입문 Day 3

---
+++ programmers/Lv0/코딩테스트입문/Day3/짝수는싫어요.py
@@ -0,0 +1,12 @@
+"""
+정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n ≤ 100
+"""
+def solution(n):
+    return list(range(1, n + 1, 2))
+
+if __name__ == '__main__':
+    solution(10)
+    solution(15)
