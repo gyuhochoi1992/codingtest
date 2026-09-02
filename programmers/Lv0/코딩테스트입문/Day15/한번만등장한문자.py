0335b8e3a67e44b6e889bee0c9e153fe098e6a70
gyuhochoi1992 <gyuhochoi1992@gmail.com>
2026-09-02 21:04:46 +0900

    [Programmers] 코딩테스트 입문 Day 15

---
+++ programmers/Lv0/코딩테스트입문/Day15/한번만등장한문자.py
@@ -0,0 +1,15 @@
+"""
+문자열 s가 매개변수로 주어집니다. s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요. 한 번만 등장하는 문자가 없을 경우 빈 문자열을 return 합니다.
+
+제한사항
+0 < s의 길이 < 1,000
+s는 소문자로만 이루어져 있습니다.
+"""
+def solution(s):
+    answer = [c for c in set(s) if s.count(c) == 1]
+    return ''.join(sorted(answer))
+
+if __name__ == '__main__':
+    solution("abcabcadc")
+    solution("abdc")
+    solution("hello")
