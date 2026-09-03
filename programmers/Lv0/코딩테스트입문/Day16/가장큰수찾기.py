e59322bbbf1c67b7072a10ffa97317afbebad61e
gyuhochoi1992 <gyuhochoi1992@gmail.com>
2026-09-03 20:45:12 +0900

    [Programmers] 코딩테스트 입문 Day 16

---
+++ programmers/Lv0/코딩테스트입문/Day16/가장큰수찾기.py
@@ -0,0 +1,16 @@
+"""
+정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+1 ≤ array의 길이 ≤ 100
+0 ≤ array 원소 ≤ 1,000
+array에 중복된 숫자는 없습니다.
+"""
+def solution(array):
+    max_val = max(array)
+    max_idx = array.index(max_val)
+    return [max_val, max_idx]
+
+if __name__ == '__main__':
+    solution([1, 8, 3])
+    solution([9, 10, 11, 8])
