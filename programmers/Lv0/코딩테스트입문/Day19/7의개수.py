594d5ca5d5d1469b0cf7517a1bfeb8bf144a1b27
gyuhochoi1992 <gyuhochoi1992@gmail.com>
2026-09-07 20:49:24 +0900

    [Programmers] 코딩테스트 입문 Day 19

---
+++ programmers/Lv0/코딩테스트입문/Day19/7의개수.py
@@ -0,0 +1,17 @@
+"""
+머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+1 ≤ array의 길이 ≤ 100
+0 ≤ array의 원소 ≤ 100,000
+"""
+def solution(array):
+    return "".join(map(str, array)).count('7')
+
+    """
+    return sum(str(s).count('7') for s in array)
+    """
+
+if __name__ == '__main__':
+    solution([7, 77, 17])
+    solution([10, 29])
